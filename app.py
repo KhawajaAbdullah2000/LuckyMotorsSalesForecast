6a247142e0ad8434a160196972ac0111a4ba7ea1
k200385 <k200385@nu.edu.pk>
2024-07-06 18:03:48 +0500

    fixed the history

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import pickle
 from flask_cors import CORS 
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)  
@@ -29,8 +30,19 @@ def predict():
         #witout historic data
         df_future = model.make_future_dataframe(df, periods=periods)
 
-        # df_future = model.make_future_dataframe(df, n_historic_predictions=True, periods=periods)
         forecast = model.predict(df_future)
+        # filename = f'forecast_{datetime.now().strftime("%Y%m%d%H%M%S")}.xlsx'
+        # forecast.to_excel(filename, index=False)
+        #original Sales data2017
+        # df_original=pd.read_csv('Car_Sales_2017.csv')
+        # forecast['error'] = (forecast['yhat1'] - df_original['y']).abs() / df_original['y']
+     
+        # forecast['error_percentage'] = forecast['error'] * 100
+        
+        # mape = forecast['error'].mean() * 100
+        # print("Mean absolute Percentage error is: ",mape)
+        
+        
         return jsonify(forecast.to_dict())
     except Exception as e:
         print("error" ,str(e))
@@ -51,13 +63,14 @@ def predict_history():
         df = pd.read_csv(file)
         df=df.iloc[:, :2]
         df['ds'] = pd.to_datetime(df['ds'], format='%d/%m/%Y')
-        model.restore_trainer()
+       # model.restore_trainer()
         #witout historic data
         #df_future = model.make_future_dataframe(df, periods=periods)
 
-        df_future = model.make_future_dataframe(df, n_historic_predictions=True, periods=periods)
-        forecast = model.predict(df_future)
-        return jsonify(forecast.to_dict())
+        #df_future = model.make_future_dataframe(df, n_historic_predictions=True, periods=periods)
+        #forecast = model.predict(df_future)
+       
+        return jsonify(df.to_dict())
     except Exception as e:
         print("error" ,str(e))
         return jsonify({"error": str(e)}), 500
